4a6d51b0d3411506be8f8dcd096f112137916a9b
Leejeongwoo <juny1alex@gmail.com>
2024-06-02 21:08:35 +0900

    feat: product register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const authController = {};
 
 authController.loginWithEmail = async (req, res) => {
@@ -15,6 +16,32 @@ authController.loginWithEmail = async (req, res) => {
       }
     }
     throw new Error("invalid email or password");
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) throw new Error("Token not found");
+    const token = tokenString.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (error, payload) => {
+      if (error) throw new Error("invalid token");
+      req.userId = payload._id;
+    });
+    next();
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+authController.checkAdminPermission = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+    if (user.level !== "admin") throw new Error("no permission");
+    next();
   } catch (error) {
     res.status(400).json({ status: "fail", message: error.message });
   }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userApi = require("./user.api");
 const authApi = require("./auth.api");
+const productApi = require("./product.api");
 const router = express.Router();
 
 router.use("/user", userApi);
 router.use("/auth", authApi);
+router.use("/product", productApi);
 module.exports = router;
